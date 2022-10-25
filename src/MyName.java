27fb4fb09c6cef0e1e211deda3cd9901f406a0f1
Marcin <lmartino@wp.pl>
2022-10-25 09:44:19 +0200

    Update MyName.java
    Aktualizacja

--- src/MyName.java
+++ src/MyName.java
@@ -1,6 +1,6 @@
 public class MyName {
     public static void main(String[] args) {
         String name = "Marcin";
-        System.out.println("Moje imię to: " + name);
+        System.out.println("Mam na imię: " + name);
     }
 }
